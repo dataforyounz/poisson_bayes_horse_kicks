4f8407efac0de5dd857c95e3f0eb39a3222a842b
dataforyounz <taylor.rt17@gmail.com>
2022-12-03 21:55:19 +1300

    hierarchical jags added

--- bayes.R
+++ bayes.R
@@ -36,5 +36,27 @@ hist( posterior$samples, nclass = 50, freq = F)
 lam <- seq(0, 5, by = .01)
 lines( lam, dgamma(lam, alpha + sum(deaths), beta + n_obs), col = "red")
 
+## Hierarchical 
 
+deaths <- data_long %>% pull(Deaths)
+corps  <- data_long %>% distinct( Corp ) %>% pull()
+n_obs  <- data_long %>% distinct( Year ) %>% count() %>% pull()
+n_corp <- length( corps )
+
+deaths <- matrix( deaths, ncol = n_corp, nrow = n_obs )
+
+tmp <- read_csv( paste0("data/", data_file), col_types = cols() ) 
+
+death_mat <- as.matrix(tmp)[,-1]
+
+data   <- c("death_mat", "n_obs", "n_corp")
+params <- c("lambda", "alpha", "beta")
+
+model_fit <- jags( data, parameters.to.save = params, model.file = "src/model_conjugate_hier.txt",
+                   n.chains = 5, n.iter = 5000, n.burnin = 2500, n.thin = 1, DIC = T)
+
+model_fit
+
+hist( model_fit$BUGSoutput$sims.list$alpha, nclass = 50 )
+hist( model_fit$BUGSoutput$sims.list$beta, nclass = 50 )
 
