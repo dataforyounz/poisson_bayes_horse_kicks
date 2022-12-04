48b8a83006e79ec02cd2224f1b546d9665876e0a
dataforyounz <taylor.rt17@gmail.com>
2022-12-04 16:22:23 +1300

    plots added

--- bayes.R
+++ bayes.R
@@ -11,30 +11,52 @@ data_file <- list.files( "data" )
 data_long <- read_csv( paste0("data/", data_file), col_types = cols() ) %>%
              pivot_longer( -Year, names_to = "Corp", values_to = "Deaths" )
 
-
 ## Pooled model --------------------------------------------------------
 
 deaths <- data_long %>% pull(Deaths)
 n_obs  <- length( deaths ) 
 
-data   <- c( "n_obs", "deaths" )
-params <- "lambda"
+# Priors on Gamma distribution - already specified in .txt file.
+alpha <- 1
+beta  <- 1
 
-model_fit <- jags( data, parameters.to.save = params, model.file = "src/model_conjugate.txt",
-                  n.chains = 2, n.iter = 5000, n.burnin = 1000, n.thin = 1, DIC = T)
+# Specify jags model and generate samples
+data      <- c( "n_obs", "deaths" )
+params    <- "lambda"
+model_fit <- jags( data, 
+                   parameters.to.save = params, 
+                   model.file = "src/model_conjugate.txt",
+                   n.chains = 2, 
+                   n.iter = 5000, 
+                   n.burnin = 1000, 
+                   n.thin = 1, 
+                   DIC = T)
 
-model_fit
-
+# Create tibble of posterior samples
 posterior <- tibble(
              samples = model_fit$BUGSoutput$sims.list$lambda[,1]
 )
 
-ggplot() + geom_histogram( data = posterior, aes(x = samples), bins = 30, fill = "light blue", col = "blue") 
+# Histogram of posterior with Gamma density overlayed
+plot_pooled <- ggplot( data = posterior ) + 
+               geom_histogram( aes(x = samples, y = after_stat(density) ), 
+                               bins = 35, fill = "sky blue", col = "white" ) +
+               geom_function( fun = dgamma, 
+                              args = list( shape = alpha + sum(deaths), rate = beta + n_obs), 
+                              col = "navy", size = .7) +
+               labs( x = "Lambda", y = "Density", subtitle = "Posterior samples") +
+               theme_bw() 
 
-hist( posterior$samples, nclass = 50, freq = F)
+ggsave( plot_pooled, file = "figs/plot_pooled.png", units = "cm", width = 8, height = 7)
 
-lam <- seq(0, 5, by = .01)
-lines( lam, dgamma(lam, alpha + sum(deaths), beta + n_obs), col = "red")
+
+index <- sample( 1:dim(model_fit$BUGSoutput$sims.list$lambda)[1], 10, replace = T)
+samps <- model_fit$BUGSoutput$sims.list$lambda[index,1]
+
+sapply( samps, function(lambda) dpois(0:10, lambda) ) 
+
+
+
 
 ## Hierarchical 
 
