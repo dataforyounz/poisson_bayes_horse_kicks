cceac280e31c28ef774d48a7de183e869770013c
dataforyounz <taylor.rt17@gmail.com>
2023-01-08 08:58:29 +1300

    Added new analytics file

--- analysis.R
+++ analysis.R
@@ -3,6 +3,7 @@ rm( list = ls() )
 library( tidyverse )
 library( gridExtra )
 library( ggpubr )
+library( kableExtra )
 
 ## -----------------------------------------------------------------------------
 ## Frequentist analysis of horse kick data
@@ -43,5 +44,47 @@ ggsave( plot_mle_pooled, file = "figs/plot_mle_pooled.png", units = "cm", width 
 ## Fit to each Corp ------------------------------------------------------------
 
 
+data_corp <- data_long %>%
+             group_by( Corp ) %>%
+             count( Deaths ) %>%
+             left_join( mle_corps, by = "Corp") %>%
+             mutate( freq = n / sum(n), 
+                     exp = dpois(0:max(Deaths), mean), 
+                     exp_n = exp * sum(n), 
+                     n_obs = sum(n),
+                     n_deaths = mean * n_obs ) %>%
+             ungroup()
+
+stat_corp <- data_corp %>%
+             select( Corp, n_deaths, n_obs, mean ) %>%
+             distinct( .keep_all = TRUE ) %>%
+             rename( Deaths = n_deaths, 
+                     "Total Obs" = n_obs, 
+                     Rate = mean )
+
+plot_mle_corp <- data_corp %>%
+                 ggplot( aes(x = Deaths, y = freq)) +
+                 geom_point( aes(col = "Data") ) +
+                 geom_line( aes( y = exp)) +
+                 geom_point( aes( y = exp, col = "Fit") ) +
+                 facet_wrap(. ~ Corp, scales = "fixed", nrow = 2) +
+                 labs( y = "Density", col = NULL, subtitle = "Poisson approximation of horse kick deaths by Corp") +
+                 theme_bw() +
+                 theme( legend.title = element_blank(), 
+                        legend.position = "bottom") +
+                 scale_color_manual( values = c("firebrick", "black"))
+
+ggsave( plot_mle_corp, file = "figs/plot_mle_corp.png", units = "cm", width = 20, height = 10)
+
+# Table
+stat_corp %>% 
+  select(1:4) %>% 
+  kbl( table.attr = "style='width:50%;'",
+       caption = "Observed deaths across corps and goodness of fit with Poisson distirbution.") %>% 
+  kable_classic( full_width = T ) %>%
+  column_spec( 4, color = "white", 
+               background = spec_color(stat_corp$Rate[1:14], end = 0.5)) %>%
+  footnote( general = "GOF = Chi Square Goodness of Fit Statistic.", footnote_as_chunk = T ) %>%
+  save_kable( "figs/table_mle_corp.png", zoom = 10 )
 
 
